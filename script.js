5aa0418dfcee59c4012636a60d38c25eab03b56b
Arselino Unisse <206115781+arselinounisse@users.noreply.github.com>
2025-09-27 18:12:21 +0200

    pussy

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function tema(){
+    var bunda = document.querySelector('div#tema');
+    bunda.addEventListener('click', tema);
+
+    var xixi = document.body;
+
+   xixi.classList.toggle('claro')
+
+};
+
+
